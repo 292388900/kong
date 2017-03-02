587144308f37a5f6908026bb17d80ec04635cb05
Robert Paprocki <robert@cryptobells.com>
2017-03-02 12:22:55 -0800

    fix(hmac-auth) generate a credential secret if none provided
    Since the credential secret is required to compute the signature,
    create a random secret which will be displayed back to the user
    as part of the response body.
    
    This fixes issue #2143.

--- kong/plugins/hmac-auth/daos.lua
+++ kong/plugins/hmac-auth/daos.lua
@@ -1,3 +1,9 @@
+local utils = require "kong.tools.utils"
+
+local function random_secret(t)
+  return utils.random_string()
+end
+
 local SCHEMA = {
   primary_key = {"id"},
   table = "hmacauth_credentials",
@@ -6,7 +12,7 @@ local SCHEMA = {
     created_at = {type = "timestamp", immutable = true, dao_insert_value = true},
     consumer_id = {type = "id", required = true, foreign = "consumers:id"},
     username = {type = "string", required = true, unique = true},
-    secret = {type = "string"}
+    secret = {type = "string", default = random_secret}
   },
   marshall_event = function(self, t)
     return {id = t.id, consumer_id = t.consumer_id, username = t.username}
